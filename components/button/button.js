0c14ea699f1306aae97d0c57900936d6add7f1c2
boyantas <122317217+boyantas@users.noreply.github.com>
2026-04-09 12:51:42 +0200

    Update button.js

--- components/button/button.js
+++ components/button/button.js
@@ -2,3 +2,18 @@
 export const DeepButton = () => {
   return <button className="deep-button">Click Me</button>;
 };
+import React from 'react';
+import './button.css';
+
+/**
+ * DeepButton component based on Figma designs
+ */
+export const DeepButton = ({ label = "Button", onClick }) => {
+  return (
+    <button className="deep-button" onClick={onClick}>
+      {label}
+    </button>
+  );
+};
+
+export default DeepButton;
